ccf4ed76e937e60cf5766d4d959972f89f7eda26
Rohit Kumar <rohit@thinkjs.club>
2023-09-18 00:50:00 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+
+const io = new socketIo.Server({
+    cors: {
+        origin: '*'
+    }
+});
+
+// new socket.Server(this.httpServer, {
+//     cors: {
+//         origin: '*'
+//     },
+//     pingInterval: 5000,
+//     pingTimeout: 20000
+// })
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('message', (message) => {
+        io.emit('message', message); // Broadcast the message to all connected clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+io.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
