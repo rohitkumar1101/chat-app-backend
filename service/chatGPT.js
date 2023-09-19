c24a18bcaf23b3e16cad141f3cf8ad15c2d70cf7
Rohit Kumar <rohit@thinkjs.club>
2023-09-19 16:14:39 +0530

    API endpoint added

--- service/chatGPT.js
+++ service/chatGPT.js
@@ -1,8 +1,20 @@
-const sendMessage = (req, res) => {
+const OpenAI = require('openai')
+require('dotenv').config();
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+const sendMessage = async (req, res) => {
     try {
         let data = req.body
-        console.log('data: ', data);
-        return res.status(200).send(data)
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: 'user', content: data.message }],
+            model: 'gpt-3.5-turbo',
+        });
+
+        return res.status(200).send(completion.choices)
     } catch (error) {
         console.error(error)
     }
